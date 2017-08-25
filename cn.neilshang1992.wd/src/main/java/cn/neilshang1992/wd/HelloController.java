cb96621c097fc13b9d2ec92a6ddaf91c43c0887b
neilshang <449876302@qq.com>
2017-08-25 18:04:33 +0800

    update

---
+++ cn.neilshang1992.wd/src/main/java/cn/neilshang1992/wd/HelloController.java
@@ -0,0 +1,13 @@
+package cn.neilshang1992.wd;
+    import org.springframework.web.bind.annotation.RequestMapping;  
+    import org.springframework.web.bind.annotation.RestController;  
+      
+    @RestController  
+    public class HelloController {  
+          
+        @RequestMapping("/hello")  
+        public String hello() {  
+            return "hello world";  
+        }  
+      
+    }  
---
+++ cn.neilshang1992.wd/src/main/java/cn/neilshang1992/wd/WebConfig.java
@@ -0,0 +1,9 @@
+package cn.neilshang1992.wd;
+import org.springframework.context.annotation.Configuration;  
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;  
+  
+@Configuration  
+@EnableWebMvc  
+public class WebConfig {  
+  
+} 
